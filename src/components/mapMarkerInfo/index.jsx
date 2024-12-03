1132b52454688462f3aaf60e989481f8c0bd3fec
ardanclassic <ardan.classic@gmail.com>
2024-12-03 23:28:40 +0700

    Updates -- UI Updates & Popup, Grouping Same Popup Coordinates

--- src/components/mapMarkerInfo/index.jsx
+++ src/components/mapMarkerInfo/index.jsx
@@ -2,25 +2,59 @@ import React, { useEffect, useState } from "react";
 import { MarkerF, OverlayView } from "@react-google-maps/api";
 import pinpoint from "assets/icons/pinpoint.png";
 import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import SwalPopup from "components/swalPopup";
+
 import "./style.scss";
 
-const MarkerInfo = ({ location, showAllInfo }) => {
+const Popup = withReactContent(Swal);
+
+const MarkerInfo = ({ group, location, showAllInfo }) => {
   const ShowNotes = () => {
     if (location.notes && location.notes !== "Cp : -") {
-      return <div className="notes bg-[#9fcaf3] p-2">{location.notes}</div>;
+      return <div className="notes p-2">{location.notes}</div>;
     } else {
       return <></>;
     }
   };
 
+  if (group.length > 1) {
+    return group.map((info) => {
+      return (
+        <div
+          className={`marker-info relative left-8 -top-[200px] min-w-[300px] flex flex-col mb-2 text-sm text-gray-800 bg-[#ffe7be] shadow-[inset_0_0_20px_-8px_#000] rounded-lg overflow-hidden ${
+            showAllInfo ? "opacity-100 scale-100 visible" : "opacity-0 scale-95 invisible"
+          }`}
+        >
+          <div className="title font-semibold p-2">{info.topic}</div>
+          <div className="content p-2">
+            <div className="speaker">
+              <span className="font-semibold mr-1">Pemateri:</span>
+              {info.speaker}
+            </div>
+            <div className="time">
+              <span className="font-semibold mr-1">Waktu:</span>
+              {info.time_start} - {info.time_end}
+            </div>
+            <div className="location">
+              <span className="font-semibold mr-1">Tempat:</span>
+              {info.loc_name}
+            </div>
+          </div>
+          <ShowNotes />
+        </div>
+      );
+    });
+  }
+
   return (
     <div
-      className={`marker-info min-w-[300px] absolute -left-[130px] -bottom-[24px] flex flex-col text-sm text-gray-800 bg-white rounded-lg shadow-lg transform -translate-y-1/2 transition-opacity duration-300 overflow-hidden ${
+      className={`marker-info min-w-[300px] absolute -left-[130px] -bottom-[24px] flex flex-col text-sm text-gray-800 bg-[#ffe7be] shadow-[inset_0_0_20px_-8px_#000] rounded-lg transform -translate-y-1/2 transition-opacity duration-300 overflow-hidden ${
         showAllInfo ? "opacity-100 scale-100 visible" : "opacity-0 scale-95 invisible"
       }`}
     >
-      <div className="title font-semibold bg-[#87cefa] p-2">{location.topic}</div>
-      <div className="content bg-[#b3d9ff] p-2">
+      <div className="title font-semibold p-2">{location.topic}</div>
+      <div className="content p-2">
         <div className="speaker">
           <span className="font-semibold mr-1">Pemateri:</span>
           {location.speaker}
@@ -41,28 +75,31 @@ const MarkerInfo = ({ location, showAllInfo }) => {
 
 // Custom Modal to show location details
 const ShowPopupInfo = ({ location, onClose }) => {
-  Swal.fire({
-    html: `
-      <div style="display: flex; flex-direction: column; gap: 6px; text-align: left;">
-        <div className="title">${location.topic}</div>
-        <div className="time">${location.time_start} - ${location.time_end}</div>
-        <div className="place">${location.loc_name}</div>
-        <div className="speaker">${location.speaker}</div>
-        <div className="notes">${location.notes}</div>
-      </div>
-    `,
-    icon: "info",
-    showCancelButton: true,
-    confirmButtonText: "Buka di Google Maps",
-    cancelButtonText: "Syukron",
+  Popup.fire({
+    html: <SwalPopup type={"kajian"} info={location} close={() => Popup.close()} />,
+    showConfirmButton: false,
+    allowOutsideClick: false,
     preConfirm: () => {
       window.open(location.gmaps_url, "_blank"); // Open Google Maps URL on confirm
     },
   });
 };
 
-export const MarkerWithInfo = ({ location, showAllInfo }) => {
-  console.log(location);
+export const MarkerWithInfo = ({ location, showAllInfo, locations }) => {
+  // Buat function untuk mencari lokasi dengan lat dan lng tertentu
+  const groupTopicsByLocation = (targetLat, targetLng, locations) => {
+    // Filter lokasi berdasarkan lat dan lng
+    const sameLocations = locations.filter((location) => location.lat === targetLat && location.lng === targetLng);
+
+    // Jika ditemukan lokasi yang sama, ambil topiknya
+    if (sameLocations.length > 0) {
+      return sameLocations.map((location) => location);
+    } else {
+      return []; // Tidak ada lokasi yang cocok
+    }
+  };
+
+  const group = groupTopicsByLocation(location.lat, location.lng, locations);
 
   return (
     <>
@@ -79,7 +116,7 @@ export const MarkerWithInfo = ({ location, showAllInfo }) => {
             position={{ lat: location.lat, lng: location.lng }}
             mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
           >
-            <MarkerInfo location={location} showAllInfo={showAllInfo} />
+            <MarkerInfo group={group} location={location} showAllInfo={showAllInfo} />
           </OverlayView>
         )}
       </MarkerF>
