b053e572cd61762003ebd4ade7c3911b4632b919
derry <derryprata@gmail.com>
2024-12-07 11:37:28 +0900

    add timestamp to request

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axiosInstance from './axiosInstance';
 
 export const GET_ALL_KAJIAN = async () => {
   try {
-    const response = await axiosInstance.get('/schedule');
+    // const response = await axiosInstance.get('/schedule');
+    const response = await axiosInstance.get(`/schedule?timestamp=${new Date().getTime()}`);
     return response.data;
   } catch (error) {
     console.error('Error fetching data:', error);
