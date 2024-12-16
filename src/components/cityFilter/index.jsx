83a3444d8cfd902f54d6fb2cc743a7c171fc612f
codesan-git <satrianugrahacode@gmail.com>
2024-12-16 08:24:57 +0700

    filter city, filter category, set position, floating button

---
+++ src/components/cityFilter/index.jsx
@@ -0,0 +1,34 @@
+import PropTypes from "prop-types";
+
+const CityFilter = ({ cities, selectedCity, onCityChange }) => {
+  return (
+    <div className="mb-4">
+      <label
+        htmlFor="category-select"
+        className="block text-sm font-medium text-gray-700 mb-1"
+      >
+        Pilih Kota:
+      </label>
+      <select
+        value={selectedCity}
+        onChange={(e) => onCityChange(e.target.value)}
+        className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 text-black"
+      >
+        <option value="">All Cities</option>
+        {cities.map((city) => (
+          <option key={city} value={city}>
+            {city}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+CityFilter.propTypes = {
+  cities: PropTypes.arrayOf(PropTypes.string).isRequired,
+  selectedCity: PropTypes.string.isRequired,
+  onCityChange: PropTypes.func.isRequired,
+};
+
+export default CityFilter;
