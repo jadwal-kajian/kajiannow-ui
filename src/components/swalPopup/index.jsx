7b54c09aa352f8eb71ab79c095424cf078c4faf8
ardanclassic <ardan.classic@gmail.com>
2024-12-18 21:44:33 +0700

    UI Updates & Optimization -- v1.0

--- src/components/swalPopup/index.jsx
+++ src/components/swalPopup/index.jsx
@@ -9,7 +9,10 @@ import {
   faMapLocationDot,
   faPhone,
   faTimes,
+  faClock,
 } from "@fortawesome/free-solid-svg-icons";
+import { convertDateTime } from "../../utils/helpers";
+import CityFilter from "../../components/cityFilter";
 import "./style.scss";
 
 const timeStartMapping = {
@@ -21,9 +24,8 @@ const timeStartMapping = {
 };
 
 function SwalPopup(data) {
-  const { type, info, group, close } = data;
-
-  console.log(`dikasi info`, data);
+  const { type, info, group, close, filter } = data;
+  // console.log(data);
 
   const openGoogleMaps = (info) => {
     if (info.gmaps_url) {
@@ -45,7 +47,7 @@ function SwalPopup(data) {
       return (
         <div className="relative max-h-[500px] overflow-y-auto flex flex-col text-center text-base py-2 bg-custom-yellow-1 shadow-[inset_0_0_20px_-2px_#000]">
           <button
-            className="sticky top-3 right-6 ml-auto px-2 p-[6px] bg-custom-yellow-4 text-gray-600 hover:text-gray-800 rounded-full flex items-center justify-center z-10"
+            className="sticky top-3 right-6 ml-auto px-2 p-[6px] bg-custom-yellow-4 text-gray-600 hover:text-gray-800 rounded-full flex items-center justify-center z-10 shadow-[0_0_8px_-4px_#000]"
             onClick={close}
           >
             <FontAwesomeIcon icon={faTimes} size="lg" />
@@ -53,10 +55,8 @@ function SwalPopup(data) {
 
           {group.map((info, i) => (
             <div key={i} className="group-item mx-3 mb-4">
-              <div className="relative md:mx-6 px-3 py-[45px] pb-[50px] bg-custom-yellow-3 rounded-xl overflow-hidden">
-                <div className="title absolute left-0 top-0 w-full text-sm font-semibold px-1 py-2 bg-custom-yellow-3">
-                  {info.topic}
-                </div>
+              <div className="relative md:mx-6 px-3 pb-[50px] bg-custom-yellow-3 rounded-xl overflow-hidden">
+                <div className="title text-sm font-semibold p-2">{info.topic}</div>
 
                 <div className="content flex flex-col gap-[5px]">
                   <div className="flex gap-3 items-center">
@@ -69,6 +69,10 @@ function SwalPopup(data) {
                   </div>
                   <div className="flex gap-3 items-center">
                     <FontAwesomeIcon icon={faCalendar} className="w-4 h-4 text-gray-700 flex-shrink-0" />
+                    <span className="text-[13px] text-left text-gray-800">{convertDateTime(info.date)}</span>
+                  </div>
+                  <div className="flex gap-3 items-center">
+                    <FontAwesomeIcon icon={faClock} className="w-4 h-4 text-gray-700 flex-shrink-0" />
                     <span className="text-[13px] text-left text-gray-800">
                       {timeStartMapping[info.time_start] || info.time_start} - {info.time_end || "Selesai"}
                     </span>
@@ -117,6 +121,10 @@ function SwalPopup(data) {
             </div>
             <div className="flex gap-3 items-center">
               <FontAwesomeIcon icon={faCalendar} className="w-4 h-4 text-gray-700 flex-shrink-0" />
+              <span className="text-[13px] text-left text-gray-800">{convertDateTime(info.date)}</span>
+            </div>
+            <div className="flex gap-3 items-center">
+              <FontAwesomeIcon icon={faClock} className="w-4 h-4 text-gray-700 flex-shrink-0" />
               <span className="text-sm text-left text-gray-800">
                 {timeStartMapping[info.time_start] || info.time_start} - {info.time_end || "Selesai"}
               </span>
@@ -150,6 +158,22 @@ function SwalPopup(data) {
         </div>
       );
     }
+  } else if (type == "filter") {
+    console.log(data);
+    return (
+      <div className="relative max-h-[500px] overflow-y-auto flex flex-col text-center text-base py-2 bg-custom-yellow-1 shadow-[inset_0_0_20px_-2px_#000]">
+        <button
+          className="sticky top-3 right-6 ml-auto px-2 p-[6px] bg-custom-yellow-4 text-gray-600 hover:text-gray-800 rounded-full flex items-center justify-center z-10 shadow-[0_0_8px_-4px_#000]"
+          onClick={close}
+        >
+          <FontAwesomeIcon icon={faTimes} size="lg" />
+        </button>
+        <div className="content p-3 max-w-[90%] md:max-w-full mx-auto flex flex-col gap-2 text-[13px] md:text-base">
+          <CityFilter cities={filter.cities} selectedCity={filter.selectedCity} onCityChange={filter.onCityChange} />
+          {/* <CategoryFilter selectedCategories={selectedCategories} onCategoryChange={onCategoryChange} /> */}
+        </div>
+      </div>
+    );
   } else {
     return (
       <div className="relative flex flex-col text-center text-base py-2 bg-custom-yellow-1 shadow-[inset_0_0_20px_-2px_#000]">
