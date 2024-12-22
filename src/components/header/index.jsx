aee1701e9d279c86567a7b9e7f4c8254a9dbfd21
ardanclassic <ardan.classic@gmail.com>
2024-12-22 15:59:41 +0700

    UPDATES: header, logo, title, datepicker header format, dynamic tags category, tag info at popup

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,10 +1,15 @@
-import logo from "assets/images/logo.png";
+import logo_only from "assets/images/logo_only.png";
+import logo_horizontal from "assets/images/logo_horizontal.png";
 
 function Header() {
   return (
-    <header className="mb-3 text-center md:text-[16px] text-sm">
+    <header className="relative flex justify-between items-center pt-[6px] mb-3 text-center md:text-[16px] text-sm">
       <div className="logo">
-        <img src={logo} alt="kajiannow" className="mx-auto w-[70%] md:w-[250px]" />
+        <img src={logo_horizontal} alt="kajiannow" className="absolute top-0 w-[150px] hidden md:block rounded-3xl" />
+        <img src={logo_only} alt="kajiannow" className="absolute top-0 w-[36px] block md:hidden rounded-3xl" />
+      </div>
+      <div className="title w-full text-custom-yellow-1 font-semibold tracking-wide text-sm md:text-base">
+        Info Kajian Sunnah Indonesia
       </div>
     </header>
   );
