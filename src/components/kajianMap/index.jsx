d044ad202a0bebc0a76fc3942529863d95e50aa5
yuhanas yulianto <yulianto.yuhanas@gmail.com>
2024-12-26 07:05:10 +0700

    fix animate map when location changed

--- src/components/kajianMap/index.jsx
+++ src/components/kajianMap/index.jsx
@@ -1,16 +1,62 @@
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 import PropTypes from "prop-types";
 import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 import UserMarker from "./UserMarker";
 import KajianMarker from "./KajianMarker";
+import { useEffect, useState } from "react";
 
 const KajianMap = forwardRef(({ locations, showAllInfo, center }, ref) => {
+  const mapInstance = useRef(null);
+  const [userLocation, setUserLocation] = useState(null);
+
+  console.log("KajianMap center:", center);
+
+  useEffect(() => {
+    if (mapInstance.current) {
+      mapInstance.current.flyTo(center, 12);
+    }
+  }, [center]);
+
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setUserLocation([latitude, longitude]);
+        },
+        (error) => {
+          console.error("Error getting current location:", error);
+        },
+        {
+          enableHighAccuracy: true,
+          timeout: 10000,
+          maximumAge: 0,
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
+  useImperativeHandle(
+    ref,
+    () => ({
+      setCenter: (coords) => {
+        if (mapInstance.current) {
+          console.log("Setting center to:", coords);
+          mapInstance.current.setView(coords, 12);
+        }
+      },
+    }),
+    []
+  );
+
   return (
     <div>
       <MapContainer
-        ref={ref}
+        ref={mapInstance}
         style={{
           width: "100%",
           height: "calc(80vh - 185px)",
@@ -20,12 +66,21 @@ const KajianMap = forwardRef(({ locations, showAllInfo, center }, ref) => {
         center={center}
         zoom={12}
         scrollWheelZoom={false}
+        eventHandlers={{
+          locationfound: (e) => {
+            setUserLocation([e.latlng.lat, e.latlng.lng]);
+          },
+          locationerror: (e) => {
+            console.error("Error getting user location:", e);
+          },
+        }}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <UserMarker position={center} />
+        {userLocation && <UserMarker position={userLocation} />}
+
         {locations.map((location) => (
           <KajianMarker key={location.id} location={location} locations={locations} showAllInfo={showAllInfo} />
         ))}
